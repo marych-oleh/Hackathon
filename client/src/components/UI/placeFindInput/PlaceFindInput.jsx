4c59494d723d3c2490f65c2fa324fdd962c3515a
Oleh <oleh.marych.dev@gmail.com>
2024-04-28 00:00:13 +0300

    Added Place Input

---
+++ client/src/components/UI/placeFindInput/PlaceFindInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './PlaceFindInput.scss';
+import Autocomplete from 'react-google-autocomplete';
+
+/**
+ *
+ * @param {Object} Props
+ * @param {string} Props.className
+ * @param {(place: google.maps.places.PlaceResult) => {}} Props.onPlaceSelected
+ * @returns
+ */
+const PlaceFindInput = ({ className, onPlaceSelected, ...props }) => {
+	return (
+		<Autocomplete
+			{...props}
+			className={'placeInput' + (className ?? '')}
+			apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+			onPlaceSelected={onPlaceSelected}
+			options={{
+				componentRestrictions: { country: 'ua' },
+			}}
+		/>
+	);
+};
+
+export default PlaceFindInput;
