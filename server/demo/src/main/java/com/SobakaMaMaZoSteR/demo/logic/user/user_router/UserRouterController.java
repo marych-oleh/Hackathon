f6abc10c57262e623589bb2d1be8fbb2c4349cbf
vwicky <viktir.zozulka@gmail.com>
2024-04-28 23:09:38 +0300

    CORS FIXED

--- server/demo/src/main/java/com/SobakaMaMaZoSteR/demo/logic/user/user_router/UserRouterController.java
+++ server/demo/src/main/java/com/SobakaMaMaZoSteR/demo/logic/user/user_router/UserRouterController.java
@@ -2,6 +2,7 @@ package com.SobakaMaMaZoSteR.demo.logic.user.user_router;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 @RequestMapping("/api/v1/userRouter")
 public class UserRouterController {
 
